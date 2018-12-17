c8071f88376be25d9c46fe4602280892189a166f
hikmatul07 <nur.maulida@students.amikom.ac.id>
2018-12-17 16:09:45 +0700

    Kuis

---
+++ src/kuisjava/KonversiMataUang.java
@@ -0,0 +1,21 @@
+package kuisjava;
+
+import java.util.Scanner;
+
+public class KonversiMataUang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	        Scanner masukan = new Scanner(System.in);
+	        float rupiah, dolar, euro;
+	        System.out.print("Masukan Rupiah = ");
+	        rupiah = masukan.nextFloat();
+	        dolar = (float) (rupiah*0.0001);
+	        System.out.println();
+	        System.out.println("Hasil Kurs = ");
+	        System.out.println("Dolar = " + dolar);
+	  
+	}
+
+}
