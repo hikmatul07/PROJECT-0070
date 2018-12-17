dbb3d1d5af48c29684b23a28626e533a1de402ef
hikmatul07 <nur.maulida@students.amikom.ac.id>
2018-12-17 16:14:05 +0700

    Kuis

--- src/kuisjava/KonversiMataUang.java
+++ src/kuisjava/KonversiMataUang.java
@@ -15,6 +15,8 @@ public class KonversiMataUang {
 	        System.out.println();
 	        System.out.println("Hasil Kurs = ");
 	        System.out.println("Dolar = " + dolar);
+	        
+	        //bagaikan jihim
 	  
 	}
 
